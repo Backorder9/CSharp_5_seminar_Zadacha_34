6b18a19d56b6ecbf7f85f6d536562238dd5a1043
Dmitry Osipovich <dosipovich@hotmail.com>
2022-10-17 12:30:26 +0300

    Задача №34 к 5 семинару решена успешно!

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void EvenNumbersCount()
+{
+int [] num = new int[23];
+int j = 0; // Счётчик количества чётных чисел в массиве.
+List<int> myEvens = new List<int>(); // Список чётных чисел
+for (int i = 0; i < 23; i++)
+    {
+        num[i] = new Random().Next(100, 1000);
+        if (num[i] % 2 == 0)
+        {
+            j++;
+            myEvens.Add(num[i]);
+        }
+    }
+
+var evens = myEvens.ToArray();
+
+Console.WriteLine();
+Console.WriteLine("Создан массив случайных чисел: " + String.Join(", ", num) + ".");
+Console.WriteLine();
+Console.WriteLine($"Созданный массив соодержит {j} чётных чисел.");
+Console.WriteLine();
+Console.WriteLine("Список чётных чисел: " + String.Join(", ", evens) + ".");
+Console.WriteLine();
+}
+EvenNumbersCount();
